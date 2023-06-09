85a6eb77ac47b19ce7bcb7fbfc41f36f8e761eff
kevingarcia@dintinamo.com <kevingarcia@dintinamo.com>
2023-06-08 22:21:43 -0500

    Create character.dart
    Model of the entity Character.

---
+++ lib/models/character.dart
@@ -0,0 +1,53 @@
+import 'shared/thumbnail.dart';
+
+/// Super Hero Character.
+class Character {
+  final int id;
+  final String name;
+  final String description;
+  final String modifiedDate;
+  final Thumbnail thumbnail;
+
+  final int comicsAvailableCount;
+  final int storiesAvailableCount;
+  final int eventsAvailableCount;
+  final int seriesAvailableCount;
+
+  Character({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.modifiedDate,
+    required this.thumbnail,
+    this.comicsAvailableCount = 0,
+    this.storiesAvailableCount = 0,
+    this.eventsAvailableCount = 0,
+    this.seriesAvailableCount = 0,
+  });
+
+  factory Character.fromJson(Map<String, dynamic> json) {
+    return Character(
+      id: json['id'],
+      name: json['name'] ?? '',
+      description: json['description'] ?? '',
+      modifiedDate: json['modified'] ?? '',
+      thumbnail: Thumbnail.fromJson(json['thumbnail']),
+      comicsAvailableCount: json['comics']['available'],
+      storiesAvailableCount: json['stories']['available'],
+      eventsAvailableCount: json['events']['available'],
+      seriesAvailableCount: json['series']['available'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "description": description,
+        "modified": modifiedDate,
+        "thumbnail": thumbnail.toJson(),
+        "comics": {"available": comicsAvailableCount},
+        "stories": {"available": storiesAvailableCount},
+        "events": {"available": eventsAvailableCount},
+        "series": {"available": seriesAvailableCount},
+      };
+}
