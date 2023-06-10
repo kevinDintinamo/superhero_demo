e8b46b837260c4da29c9cd3c7db2e2edae99e881
kevingarcia@dintinamo.com <kevingarcia@dintinamo.com>
2023-06-10 09:13:00 -0500

    Adding Global Theme to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:superhero_demo/config/theme/app_theme.dart';
 
 void main() async {
   // Environment variables are loaded from .env file.
@@ -13,8 +14,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
