8e44e7425ac02803814d3f758a7e8cdc6d3a9147
kevingarcia@dintinamo.com <kevingarcia@dintinamo.com>
2023-06-10 11:49:37 -0500

    Update home_screen.dart
    Main Squema Structure

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,15 +1,131 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/widgets.dart';
+
 class HomeScreen extends StatelessWidget {
   static const screenName = 'home_screen';
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HOME SCREEN HERE'),
-      ),
+    final appBar = AppBar(
+      centerTitle: true,
+      title: const Text('Marvel Super Heroes'),
+    );
+
+    final bottomPageNavigationBar = BottomPageNavigationBar(
+      leftText: '1',
+      centerText: '2 / 95',
+      rightText: '3',
+      onLeftPressed: () {},
+      onRightPressed: () {},
+    );
+
+    return Scaffold(
+      appBar: appBar,
+      body: const SafeArea(child: _HomeView()),
+      bottomNavigationBar: bottomPageNavigationBar,
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final controller = PageController(viewportFraction: 0.85);
+
+    return ListView(
+      children: [
+        const SizedBox(height: 20),
+        SizedBox(
+          width: double.infinity,
+          height: 350,
+          child: PageView.builder(
+            itemCount: 10,
+            pageSnapping: true,
+            controller: controller,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) => Container(
+              // padding: const EdgeInsets.symmetric(horizontal: 10),
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              height: 100,
+              width: 100,
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.all(Radius.circular(50.0)),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(36.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'SuperHero Name',
+                style: theme.textTheme.titleMedium,
+                textAlign: TextAlign.start,
+              ),
+              Text(
+                // 'Desc.',
+                'Description ofVeniam nulla reprehenderit adipisicing non enim magna ad cillum proident.',
+                style: theme.textTheme.bodyMedium,
+                textAlign: TextAlign.start,
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                'Fecha de modificación: {2022-02-02}',
+                style: theme.textTheme.bodySmall,
+                textAlign: TextAlign.start,
+              ),
+              const SizedBox(height: 24.0),
+              const _MoreInfoActionWidgets(),
+              const SizedBox(height: 24.0),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _MoreInfoActionWidgets extends StatelessWidget {
+  const _MoreInfoActionWidgets();
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      alignment: WrapAlignment.center,
+      crossAxisAlignment: WrapCrossAlignment.center,
+      spacing: 8.0,
+      runAlignment: WrapAlignment.center,
+      children: [
+        TonalButton(
+          text: 'Series (4)',
+          iconData: Icons.ac_unit,
+          onPresed: () {},
+        ),
+        TonalButton(
+          text: 'Comics (4)',
+          iconData: Icons.book,
+          onPresed: () {},
+        ),
+        const TonalButton(
+          text: 'Stories (3)',
+          iconData: Icons.battery_6_bar_sharp,
+          onPresed: null,
+        ),
+        TonalButton(
+          text: 'Events (0)',
+          iconData: Icons.event,
+          onPresed: () {},
+        ),
+      ],
     );
   }
 }
