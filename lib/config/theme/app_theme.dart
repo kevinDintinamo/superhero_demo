3aa68e0c03f44f233251cb5efd67f6f8181f297c
kevingarcia@dintinamo.com <kevingarcia@dintinamo.com>
2023-06-10 11:47:15 -0500

    Update app_theme.dart
    Adding Custom TextStyles

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -8,6 +8,37 @@ class AppTheme {
       useMaterial3: true,
       colorSchemeSeed: marvelColor,
       visualDensity: VisualDensity.adaptivePlatformDensity,
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
+        titleMedium: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+          color: Colors.black87,
+        ),
+        titleSmall: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.black54,
+        ),
+        bodyLarge: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+          color: Colors.black38,
+        ),
+        bodyMedium: TextStyle(
+          fontSize: 14,
+          color: Colors.black,
+        ),
+        bodySmall: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.bold,
+          color: Colors.black38,
+        ),
+      ),
     );
   }
 }
