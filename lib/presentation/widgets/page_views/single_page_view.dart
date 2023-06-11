c29185ec588787c12c128167ee276853fe6dc2fd
kevingarcia@dintinamo.com <kevingarcia@dintinamo.com>
2023-06-11 15:04:44 -0500

    Update single_page_view.dart
    Using References

--- lib/presentation/widgets/page_views/single_page_view.dart
+++ lib/presentation/widgets/page_views/single_page_view.dart
@@ -1,31 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '/presentation/providers/providers.dart';
-
 class SinglePageView extends ConsumerWidget {
   /// Must provide an object that has a `thumbnail` property of type `ImagePath`.
   final List objectListWithThumbnails;
+  final StateProvider<int> subPageProvider;
   const SinglePageView({
     super.key,
     required this.objectListWithThumbnails,
+    required this.subPageProvider,
   });
 
   @override
   Widget build(BuildContext context, ref) {
-    final controller = PageController(viewportFraction: 0.85);
-    final subPage = ref.watch(characterSubPageIndexProvider);
+    final theme = Theme.of(context);
 
-    return PageView.builder(
+    final subPageIndex = ref.watch(subPageProvider);
+    final controller =
+        PageController(viewportFraction: 0.85, initialPage: subPageIndex);
+
+    final pageView = PageView.builder(
         itemCount: objectListWithThumbnails.length,
         pageSnapping: true,
         controller: controller,
         onPageChanged: (value) {
-          ref.read(characterSubPageIndexProvider.notifier).update((_) => value);
+          ref.read(subPageProvider.notifier).update((_) => value);
         },
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          var isShown = index == subPage;
+          var isShown = index == subPageIndex;
           return Opacity(
             opacity: isShown ? 1 : 0.5,
             child: Container(
@@ -39,6 +42,45 @@ class SinglePageView extends ConsumerWidget {
             ),
           );
         });
+
+    final paginationText = _UnderPagination(
+      theme: theme,
+      subPageProvider: subPageProvider,
+      objectListWithThumbnails: objectListWithThumbnails,
+    );
+
+    return Column(
+      children: [
+        const SizedBox(height: 20),
+        Expanded(child: pageView),
+        paginationText,
+        const SizedBox(height: 4.0),
+      ],
+    );
+  }
+}
+
+class _UnderPagination extends ConsumerWidget {
+  const _UnderPagination({
+    required this.theme,
+    // required this.subPageIndex,
+    required this.subPageProvider,
+    required this.objectListWithThumbnails,
+  });
+
+  final ThemeData theme;
+  // final int subPageIndex;
+  final StateProvider<int> subPageProvider;
+  final List objectListWithThumbnails;
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final subPageIndex = ref.watch(subPageProvider);
+    return Text(
+      '${subPageIndex + 1}/${objectListWithThumbnails.length}',
+      style: theme.textTheme.labelSmall,
+      textAlign: TextAlign.center,
+    );
   }
 }
 
